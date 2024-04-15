858a651f7da029133f305a7e6e13e52f7d52793b
Kasia Zadurska <kzadurska@gmail.com>
2024-04-15 15:27:18 +0200

    Add gh pages workflow for deploy

--- src/sections/crop-image/CropImage.tsx
+++ src/sections/crop-image/CropImage.tsx
@@ -3,15 +3,23 @@ import "react-advanced-cropper/dist/style.css";
 import { useRef, useState } from "react";
 import { FixedCropper, FixedCropperRef } from "react-advanced-cropper";
 
+const IMAGE_URLS = [
+	"https://images.unsplash.com/photo-1695576514502-cbc0a333f2d9?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	"https://images.unsplash.com/photo-1712785021787-72b6d6c837e8?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	"https://images.unsplash.com/photo-1565769583756-fe3ffffcae49?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	"https://images.unsplash.com/photo-1712257156266-81682d7cb58c?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	"https://images.unsplash.com/photo-1712256951971-ab2b5916ddb0?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	"https://images.unsplash.com/photo-1593872423141-bb230bd352c6?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
 export const CropImage = ({ predefinedSize }: { predefinedSize: string }) => {
 	const cropperRef = useRef<FixedCropperRef>(null);
+	const pseudoRandomImageIndex = getRandomInt(0, 5);
 
 	// TODO: https://unsplash.com/developers get image from api
 	// TODO: add option to upload image
 	// generate background like canvas gradient
-	const [image] = useState(
-		"https://images.unsplash.com/photo-1599140849279-1014532882fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80"
-	);
+	const [image] = useState(IMAGE_URLS[pseudoRandomImageIndex]);
 
 	const getDimensionsFromSize = (): { width: number; height: number } => {
 		const [width, height] = predefinedSize.split("x");
@@ -58,3 +66,10 @@ export const CropImage = ({ predefinedSize }: { predefinedSize: string }) => {
 		</>
 	);
 };
+
+function getRandomInt(min: number, max: number) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+
+	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}

4652e24728da88b8bcfa2fc1716700d9fbd722ac
Kasia Zadurska <kzadurska@gmail.com>
2024-04-15 14:03:57 +0200

    Add predefined sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import "react-advanced-cropper/dist/style.css";
 
+import { useState } from "react";
+
 import { CropImage } from "./sections/crop-image/CropImage";
 
+const PREDEFINED_CROP_SIZES = ["400x400", "932x125", "1080x288", "1600x350", "1536x500"];
+
 export function App() {
+	const [predefinedSize, setPredefinedSize] = useState(PREDEFINED_CROP_SIZES[0]);
+	// TODO allow any dimensions user chooses
+
+	const selectSize = (event) => setPredefinedSize(event.target.value);
+
 	return (
 		<div className="App">
-			<CropImage />
+			<select onChange={selectSize}>
+				{PREDEFINED_CROP_SIZES.map((size) => (
+					<option key={size}>{size}</option>
+				))}
+			</select>
+			<CropImage predefinedSize={predefinedSize} />
 		</div>
 	);
 }
--- src/sections/crop-image/CropImage.tsx
+++ src/sections/crop-image/CropImage.tsx
@@ -1,12 +1,14 @@
 import "react-advanced-cropper/dist/style.css";
 
 import { useRef, useState } from "react";
-import { Cropper, CropperRef } from "react-advanced-cropper";
+import { FixedCropper, FixedCropperRef } from "react-advanced-cropper";
 
-export const CropImage = () => {
-	const cropperRef = useRef<CropperRef>(null);
+export const CropImage = ({ predefinedSize }: { predefinedSize: string }) => {
+	const cropperRef = useRef<FixedCropperRef>(null);
 
-	const [image, setImage] = useState(
+	// TODO: https://unsplash.com/developers get image from api
+	// TODO: add option to upload image
+	const [image] = useState(
 		"https://images.unsplash.com/photo-1599140849279-1014532882fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80"
 	);
 
@@ -30,11 +32,28 @@ export const CropImage = () => {
 		}
 	};
 
+	const getDimensionsFromSize = (): { width: number; height: number } => {
+		const [width, height] = predefinedSize.split("x");
+
+		return { width: parseInt(width, 10), height: parseInt(height, 10) };
+	};
+
 	return (
 		<>
 			<button onClick={onDownload}>download cropped</button>
 			<button onClick={onOpenInTab}>open in new tab</button>
-			<Cropper src={image} className={"cropper"} ref={cropperRef} />;
+			<FixedCropper
+				transformImage={{ adjustStencil: false }}
+				src={image}
+				ref={cropperRef}
+				stencilProps={{
+					handlers: false,
+					lines: true,
+					movable: true,
+					resizable: false,
+				}}
+				stencilSize={getDimensionsFromSize()}
+			/>
 		</>
 	);
 };
